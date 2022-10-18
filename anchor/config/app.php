f935ba46d2e607a9a7a85b2d7741e31f6d726c7f
Tunna Duong <tunnaduong@gmail.com>
2022-10-19 05:56:27 +0700

    Implemented Anchor - A lite weight PHP CMS

---
+++ anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'url'      => '/',
+    'index'    => '',
+    'timezone' => 'Asia/Ho_Chi_Minh',
+    'key'      => 'JuapN0Ry5GXIQYqYtylqzaHnlzFlGnR3',
+    'language' => 'en_GB',
+    'encoding' => 'UTF-8'
+];
---
+++ anchor/config/db.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'default'     => 'mysql',
+    'prefix'      => 'anchor_',
+    'connections' => [
+        'mysql' => [
+            'driver'   => 'mysql',
+            'hostname' => '103.81.85.224',
+            'port'     => '3306',
+            'username' => 'tunnaduong_love',
+            'password' => 'Tunganh2003',
+            'database' => 'tunnaduong_love',
+            'charset'  => 'utf8mb4'
+        ]
+    ]
+];
